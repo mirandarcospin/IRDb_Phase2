d679ccbf9fa59c8211d220a4fb8dffa1e676fa4b
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-11-01 22:03:15 -0600

    Initial Commit

---
+++ IRDb_Phase2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IRDb_Phase2
+//
+//  Created by Miranda Ramirez Cospin on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
